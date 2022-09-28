05225d05b716ac9f51681a225e25d399bd29e29f
Patrick <patrickhu91@gmail.com>
2022-09-28 15:58:43 -0700

    BST Visualizer
    Co-authored-by: ZQland <ZQland@users.noreply.github.com>
    Co-authored-by: Mahmoud <CodeMahmoud@users.noreply.github.com>

--- client/DSAnalyzer/visualize.ts
+++ client/DSAnalyzer/visualize.ts
@@ -1,42 +1,91 @@
 //output array for frontend
-let outputArr: any = [];
+// let outputArr: any = [];
 
 const reset = () => {
-  outputArr = [];
-};
-
-const visualize = (LL: any) => {
-  console.log('THIS IS ARG', LL);
-  console.log('running visualize');
-  // empty outputArr
   // outputArr = [];
-  const list: (string | number)[] = [];
-  // currNode is equal to head node
-  let currNode = LL.head;
-
-  let firstNull = true;
-  let counter = 0;
-  // while currNode is not null
-  while (currNode || firstNull) {
-    console.log('in visualize while loop');
-    // push value into output arr
-    if (!currNode) {
-      // outputArr.push('null');
-      list.push('null');
-    }
-    else list.push(currNode.val);
-
-    // so we ensure one null gets pushed into outputArr
-    if (!currNode) firstNull = false;
-    // set currNode equal to next node
-
-    else currNode = currNode.next;
-    // else if (currNode) currNode = currNode.next;
-    // else break;
-    counter += 1;
-    if (counter > 200) break;
-  }
-  outputArr.push(list);
+  output.outputArr = [];
 };
 
-export { outputArr, visualize, reset };
+const output: { outputArr: Array<(string | number)[]>; BST: any } = {
+  outputArr: [],
+  BST: ''
+};
+
+// const visualize = (LL: any) => {
+//   console.log('THIS IS ARG', LL);
+//   console.log('running visualize');
+//   // empty outputArr
+//   // outputArr = [];
+//   const list: (string | number)[] = [];
+//   // currNode is equal to head node
+//   let currNode = LL.head;
+
+//   let firstNull = true;
+//   let counter = 0;
+//   // while currNode is not null
+//   while (currNode || firstNull) {
+//     console.log('in visualize while loop');
+//     // push value into output arr
+//     if (!currNode) {
+//       // outputArr.push('null');
+//       list.push('null');
+//     }
+//     else list.push(currNode.val);
+
+//     // so we ensure one null gets pushed into outputArr
+//     if (!currNode) firstNull = false;
+//     // set currNode equal to next node
+
+//     else currNode = currNode.next;
+//     // else if (currNode) currNode = currNode.next;
+//     // else break;
+//     counter += 1;
+//     if (counter > 200) break;
+//   }
+//   output.outputArr.push(list);
+// };
+
+const visualize = (root: any) => {
+  output.BST = root;
+};
+
+
+class BinarySearchTree {
+  val: number;
+  left: BinarySearchTree | null;
+  right: BinarySearchTree | null;
+
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+const tree = new BinarySearchTree(10);
+tree.left = new BinarySearchTree(5);
+tree.left.left = new BinarySearchTree(3);
+tree.left.right = new BinarySearchTree(6);
+tree.right = new BinarySearchTree(14);
+
+const tester = () => {
+  const bstSum = (root: any) => {
+    let sum = 0;
+    const queue = [root];
+
+    while (queue.length > 0) {
+      visualize(root);
+      const node = queue.shift();
+      if (node.val) {
+        sum += node.val;
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    console.log('i am sum', sum);
+  };
+  bstSum(tree);
+};
+
+export { output, visualize, reset, tester };
--- client/components/Visualizer.tsx
+++ client/components/Visualizer.tsx
@@ -1,29 +1,89 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Box from './Box';
 import Row from './Row';
+import BSTRow from './BSTRow';
+
+
 
 const visualizer = (props: any) => {
-  const tempBox = [];
-  for (let i = 0; i < props.data.length; i++) {
-    tempBox.push(<Row data={props.data[i]} />);
-  }
+  const tempBox: JSX.Element[] = [];
+  const bstNodes: any = [];
+  let tempVals: any = [];
+  const LL = () => {
+    for (let i = 0; i < props.data.length; i++) {
+      tempBox.push(<Row data={props.data[i]} />);
+    }
+  };
+
+  // bstNodes = [ [rootNode], [1Left, 1Right], [2LL, 2LR, 2RL, 2RR] ]
+  const addNode = (node: any) => {
+    tempVals.push(node.val);
+  };
 
 
   /*
-          5
-        3   8
-      2  4    9
+            10 -- left 5 | right 14
+          5   14
+        3   6       
   */
-  // const bfs = (props) => {
-  //   if (props.data.tree) {
+  const BFS = (root: any) => {
 
-  //   }
-  // }
+    const queue = [root];
+    let counter = 0;
+    let maxBranches = 2;
+    while (queue.length > 0) {
+      const node = queue.shift();
+
+      if (node === 'null') {
+        counter += 2;
+        tempVals.push(node);
+      }
+
+      if (node) {
+        tempVals.push(node.val);
+      }
+
+      if (node.left) {
+        counter += 1;
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+        counter += 1;
+      }
+      if (node && !node.left) {
+        counter += 1;
+        // tempVals.push('null');
+        queue.push('null');
+      }
+      if (node && !node.right) {
+        counter += 1;
+        // tempVals.push('null');
+        queue.push('null');
+      }
+
+      console.log('this is counter', counter);
+      console.log('this is maxbranch', maxBranches);
+      if (counter >= maxBranches) {
+        counter = 0;
+        maxBranches *= 2;
+        bstNodes.push(<BSTRow data={tempVals} />);
+        tempVals = [];
+      }
+      if (counter > 10) break;
+    }
+  };
+
+  console.log('THIS PROPS ON 75', props.data.BST);
+  if (props.data.outputArr !== undefined) LL();
+  if (props.data.BST !== undefined) BFS(props.data.BST);
 
   return (
     <div className="box-container">
-      {tempBox}
+      {tempBox !== null &&
+        tempBox}
+      {bstNodes !== null &&
+        bstNodes}
     </div>
   );
 };
